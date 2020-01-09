b269492e371480e5aaed17a5e684a1114607f7f0
venkatesh <58026249+venkybavisetti@users.noreply.github.com>
2020-01-09 13:05:41 +0530

    added search bar

--- htmlGenerator.js
+++ htmlGenerator.js
@@ -9,15 +9,24 @@ const html = [
   // Bulbasaur"
   'height="250"/><div class="powerTypes">',
   //  typeCode
-  '</div></div><div class="pokemonBack"><table><tr><th>name</th><td>',
-  // name
-  "</td></tr><tr><th>id</th><td>",
+  '</div></div><div class="pokemonBack"><h3 class="frontHeader">',
+
+  //Bulbasaur
+  "</h3><img src=",
+  //http://assets22.pokemon.com/assets/cms2/img/pokedex/full/001.png"------
+  "alt=",
+  //Bulbasaur"
+  'height="150" /><center><table border="1px" class="tableDimension"><tr><th class="thProperties">id</th ><td class="tdProperties">',
   // id
-  "</td></tr><tr><th>height</th><td>",
+  '</td></tr><tr><th class="thProperties">height</th ><td class="tdProperties">',
   // height
-  "</td></tr><tr><th>weight</th><td>",
+  '</td></tr><tr><th class="thProperties">weight</th ><td class="tdProperties">',
   // weight
-  "</td></tr></table></div></div></div>"
+  '</td></tr><tr><th class="thProperties">avg_spawns</th ><td class="tdProperties">',
+  //avg_spawn
+  '</td></tr><tr><th class="thProperties">weaknesses</th ><td style="font-size: small;">',
+  //weaknesses
+  "</td></tr></table></center></div></div></div>"
 ];
 
 const getTypeCode = types => {
@@ -25,6 +34,10 @@ const getTypeCode = types => {
     code += `<div class="type"><div class="${type}"></div><span>${type}</span></div>`;
     return code;
   }, "");
+};
+
+const getWeaknesses = function(weaknesses) {
+  return weaknesses.join(",");
 };
 
 const getHtml = content => {
@@ -42,12 +55,21 @@ const getHtml = content => {
       html[4] +
       pokemon.name +
       html[5] +
-      pokemon.id +
+      `"${pokemon.art_url}"` +
       html[6] +
-      pokemon.height +
+      `"${pokemon.name}"` +
       html[7] +
+      pokemon.id +
+      html[8] +
+      pokemon.height +
+      html[9] +
       pokemon.weight +
-      html[8];
+      html[10] +
+      pokemon.avg_spawns +
+      html[11] +
+      getWeaknesses(pokemon.weaknesses) +
+      html[12];
+    //
     htmlCode.push(code);
   });
   writeFileSync("./htmlCode.html", htmlCode.join(""), "utf8");
